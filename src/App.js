2c9bc359f290c6b70db3822f28f9d5bbb390deb9
Boluwatife <omotayodaniel10@gmail.com>
2022-10-10 19:22:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavButtons from "./components/NavButtons";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <NavButtons />
+    </React.Fragment>
+  );
+};
+
+export default App;
